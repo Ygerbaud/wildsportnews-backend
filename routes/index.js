a119b5f1cfae14205f658b207c74c578b7cec756
Yohan Gerbaud <86349498+Ygerbaud@users.noreply.github.com>
2022-02-03 11:44:00 +0100

    Ajout authentifiaction

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 
 const ArticlesRouter = require('./articles');
 const UsersRouter = require('./users');
+const authRouter = require('./auth');
 
 
 
 const setupRoutes = (app) => {
+  app.use('/auth', authRouter)
   app.use('/articles', ArticlesRouter)
   app.use('/users', UsersRouter)
 }
